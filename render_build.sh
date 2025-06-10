3e880831458551b89e68102e15d13127a9fcded1
SSaiLokesh <kluniversityloki2006@gmail.com>
2025-06-10 18:17:50 +0530

    initial commit

---
+++ render_build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Download the dataset (optional – if your app needs it at build time)
+# wget -O dataset.zip https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip
+# unzip dataset.zip -d ./data
+# Clean up the downloaded zip file
+# rm dataset.zip
