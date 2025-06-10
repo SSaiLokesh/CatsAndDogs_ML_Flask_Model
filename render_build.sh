3d912ed054d844301c0da6a745aa6df787c28c02
SSaiLokesh <kluniversityloki2006@gmail.com>
2025-06-10 18:22:11 +0530

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Install Python dependencies
-pip install -r requirements.txt
+# pip install -r requirements.txt
+pip install -r requirments.txt
+
 
 # Download the dataset (optional – if your app needs it at build time)
 # wget -O dataset.zip https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip
